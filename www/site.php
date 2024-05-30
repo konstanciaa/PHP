ac630ee0232c77520533faeebc38b1b4ddde8512
Anna Konstantynova <hannakonstantinova@gmail.com>
2024-05-30 09:39:20 +0000

    working with numbers and styles

--- www/site.php
+++ www/site.php
@@ -3,7 +3,8 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>PHP</title>
+    <link rel="stylesheet" href="/css/site.css">
 </head>
 <body>
     
@@ -70,5 +71,65 @@
         echo "<br>";
     ?>
 
+    <!-- working with numbers -->
+    <?php
+        echo "<h2>Working with numbers</h2>";
+        echo "<code>echo 40;</code> Result: ";
+        echo 40;
+        echo "<br>";
+        echo "<code>echo -40;</code> Result: ";
+        echo -40;
+        echo "<br>";
+        echo "<code>echo -30.378;</code> Result: ";
+        echo -30.378;
+        echo "<br>";
+        echo "<code>echo 5 + 9;</code> Result: ";
+        echo 5 + 9;
+        echo "<br>";
+        echo "<code>echo 10 / 2;</code> Result: ";
+        echo 10 / 2;
+        echo "<br>";
+        echo "<code>echo 10 % 3;</code> Result: ";
+        echo 10 % 3; // modulo operator, remainder of devision
+        echo "<br>";
+        echo "<code>echo 4 + 5 * 10;</code> Result: ";
+        echo 4 + 5 * 10;
+        echo "<br>";
+        echo "<code>echo (4 + 5) * 10;</code> Result: ";
+        echo (4 + 5) * 10;
+        echo "<br>";
+        echo "<code>$num = 10; echo $num;</code> Result: ";
+        $num = 10;
+        echo $num;
+        echo "<br>";
+        $num++;
+        echo $num;
+        echo "<br>";
+        $num--;
+        echo $num;
+        echo "<br>";
+        $num = $num + 25; // $num += 25;
+        echo $num;
+        echo "<br>";
+        $num -= 25;
+        echo $num;
+        echo "<br>";
+        echo abs(-100); // absolute value of a number
+        echo "<br>";
+        echo pow(2, 4); // 2 raised to 4 power
+        echo "<br>";
+        echo sqrt(144); // square root
+        echo "<br>";
+        echo max(2, 10); // prints the bigger number
+        echo "<br>";
+        echo min(2, 10); // prints the smallest number
+        echo "<br>";
+        echo round(3.2); // rounds
+        echo "<br>";
+        echo ceil(3.3); // rounds up no matter what
+        echo "<br>";
+        echo floor(3.9); // rounds down no matter what
+        echo "<br>";
+    ?>
 </body>
 </html>
