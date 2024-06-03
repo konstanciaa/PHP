22c149b3f81ba171c2a02d4a7e122087053792d8
Anna Konstantynova <hannakonstantinova@gmail.com>
2024-06-03 08:00:46 +0000

    Using Checkboxes

--- www/site-two.php
+++ www/site-two.php
@@ -18,5 +18,18 @@
         echo $friends[4];
         echo count($friends); // count elements - 5
     ?>
+
+    <!-- Checkboxes -->
+    <!-- name="fruits[]" - getting multiple values into an array -->
+    <form action="site-two.php" method="post">
+        Apples: <input type="checkbox" name="fruits[]" value="apples"><br>
+        Oranges: <input type="checkbox" name="fruits[]" value="oranges"><br>
+        Pears: <input type="checkbox" name="fruits[]" value="pears"><br>
+        <input type="submit">
+    </form>
+    <?php
+        $fruits = $_POST["fruits"];
+        echo $fruits[0];
+    ?>
 </body>
 </html>
